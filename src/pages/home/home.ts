87c1d4dc6ae3aba38628092eb6c9967dc06c8206
PauloJorgeJunior <p4ulojorge@gmail.com>
2018-06-14 17:38:49 -0400

    Aula 2.8

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+import { CarrosServiceProvider } from './../../providers/carros-service/carros-service';
+import { Carro } from './../../app/modelos/carro';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros: Carro[];
 
+  constructor(public navCtrl: NavController, private _http: HttpClient,
+              private _loadingCtrl: LoadingController, private _alertCtrl: AlertController,
+              private _carrosService: CarrosServiceProvider) {
+
+    let loading = this._loadingCtrl.create({
+      content: 'Carregamento dos carros...'
+    });
+
+    loading.present();
+
+    this._carrosService.lista()
+      .subscribe(
+      (carros) => {
+        this.carros = carros;
+        loading.dismiss();
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+        loading.dismiss();
+        this._alertCtrl.create({
+          title: 'Falha na conexão',
+          subTitle: 'Não foi possível carregar a lista de carros. Tente novamente mais tarde.',
+          buttons: [
+            { text: 'Ok' }
+          ]
+        }).present();
+      }
+      );
   }
 
 }
